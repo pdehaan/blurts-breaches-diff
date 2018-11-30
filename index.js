f3009023d6a352c7824926458ff6eac39e8a0232
Peter deHaan <peter@deseloper.com>
2018-11-30 12:05:15 -0800

    V2 all the thangs

--- index.js
+++ index.js
@@ -4,46 +4,57 @@ const got = require("got").get;
 const ms = require("ms");
 const pkg = require("./package.json");
 
+const envs = new Map();
+envs.set("dev", "https://fx-breach-alerts.herokuapp.com");
+envs.set("stage", "https://blurts-server.stage.mozaws.net");
+envs.set("prod", "https://monitor.firefox.com");
+
 function args() {
-  const flags = arg({
-    "--env": String,
-    "--server": String,
-    "--since": String
-  }, {permissive: true});
+  const flags = arg(
+    {
+      "--env": String,
+      "--server": String,
+      "--since": String
+    },
+    { permissive: true }
+  );
 
   if (flags["--env"]) {
-    const env = flags["--env"];
-    switch (env) {
-      case "l10n":
-      case "dev":
-      case "development":
-        flags["--server"] = "https://fx-breach-alerts.herokuapp.com";
-        break;
-      case "stage":
-        flags["--server"] = "https://blurts-server.stage.mozaws.net";
-        break;
-      case "prod":
-      case "production":
-        flags["--sever"] = "https://monitor.firefox.com";
-        break;
-      default:
-        // eslint-disable-next-line no-console
-        console.error(`Unknown --env: ${env}`);
-    }
+    flags["--server"] = getServer(flags["--env"]);
   }
 
   return {
-    server: flags["--server"] || "https://monitor.firefox.com",
-    since: flags["--since"] || flags._[0] || "-7d"
+    server: flags["--server"] || envs.get("prod"),
+    since: flags["--since"] || flags._[0] || "-1w"
   };
 }
 
-async function getBreaches(modifiedSince="-1w", server="https://monitor.firefox.com") {
+function getServer(env) {
+  switch (env) {
+    case "l10n":
+    case "dev":
+    case "development":
+      return envs.get("dev");
+    case "stage":
+      return envs.get("stage");
+    case "prod":
+    case "production":
+      return envs.get("prod");
+    default:
+      // eslint-disable-next-line no-console
+      console.error(
+        `Unknown env: "${env}". Setting --server to ${envs.get("prod")}`
+      );
+      return envs.get("prod");
+  }
+}
+
+async function getBreaches(modifiedSince = "-1w", server = envs.get("prod")) {
   const now = new Date();
   const BREACH_API_URL = `${server}/hibp/breaches`;
   const BREACH_VERSION_URL = `${server}/__version__`;
-  const sinceMs = Math.abs(ms(modifiedSince));
-  const sinceDate = new Date(now - sinceMs);
+  const sinceMs = ms(modifiedSince);
+  const sinceDate = new Date(now - Math.abs(sinceMs));
   const res = await got(BREACH_API_URL, {
     headers: {
       "user-agent": `${pkg.name}/${pkg.version}`,
@@ -54,20 +65,23 @@ async function getBreaches(modifiedSince="-1w", server="https://monitor.firefox.
 
   let __version;
   try {
-    __version = (await got(BREACH_VERSION_URL, {json: true})).body;
+    __version = (await got(BREACH_VERSION_URL, { json: true })).body;
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(`${err.message}: ${BREACH_VERSION_URL}`);
   }
 
   // Assume we have no recent breaches. If we get a non "304" status code from the remote server these values will be updated below.
-  let breaches =[];
+  let breaches = [];
   let missingLogos = [];
 
   if (res.statusCode !== 304 && res.body) {
-    breaches = res.body.filter(({ModifiedDate}) => new Date(ModifiedDate) >= sinceDate);
-    missingLogos = (await checkLogos(breaches, server))
-      .filter(logo => logo.status !== "alive");
+    breaches = res.body.filter(
+      ({ ModifiedDate }) => new Date(ModifiedDate) >= sinceDate
+    );
+    missingLogos = (await checkLogos(breaches, server)).filter(
+      logo => logo.status !== "alive"
+    );
   }
 
   return {
@@ -86,17 +100,14 @@ async function getBreaches(modifiedSince="-1w", server="https://monitor.firefox.
 
 async function checkLogos(breaches, server) {
   const LOGO_URI_PREFIX = `${server}/img/logos`;
-  const links = breaches.reduce((_links, {Name, LogoType}) => {
-    _links.push(`${LOGO_URI_PREFIX}/${Name}.${LogoType}`);
-    if (LogoType !== "png") {
-      _links.push(`${LOGO_URI_PREFIX}/${Name}.png`);
-    }
+  const links = breaches.reduce((_links, { Name, LogoPath }) => {
+    const [img] = LogoPath.match(/[^/]*$/);
+    img && _links.push(`${LOGO_URI_PREFIX}/${img}`);
     return _links;
   }, []);
 
   const linkCheck = await checkLinks(links);
-  return Object.entries(linkCheck)
-    .map(([url, res]) => ({url, ...res}));
+  return Object.entries(linkCheck).map(([url, res]) => ({ url, ...res }));
 }
 
 module.exports = {
