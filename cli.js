034424ed1b9d49f8c348b1ab0bdf1636679f5969
Peter deHaan <peter@deseloper.com>
2018-11-30 15:17:32 -0800

    Fix arg typo in cli.js (#10)

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const flags = args();
 main(flags);
 
 async function main(opts) {
-  const recentBreaches = await getBreaches(opts.modifiedSince, opts.server);
+  const recentBreaches = await getBreaches(opts.since, opts.server);
   // eslint-disable-next-line no-console
   console.log(JSON.stringify(recentBreaches, null, 2));
 }
